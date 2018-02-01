47f4b5fd3abd45ae13eecda91d848f11be37ac5a
Chuang Lagrant <chuanglagrant@gmail.com>
2018-02-01 10:07:32 +0800

    build halfedge on input model

--- main.cpp
+++ main.cpp
@@ -1,54 +1,111 @@
 #include <igl/readOFF.h>
 #include <igl/viewer/Viewer.h>
 #include <igl/per_face_normals.h>
+#include <Eigen/LU>
 #include <iostream>
 #include <vector>
 #include "set.h"
 #include "constraints.h"
+#include "halfedge.h"
+
+#define random(x) (rand()%x)
 
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
 Eigen::MatrixXd N_faces;
+Eigen::Vector3d* d;
+set<int>* components;
+set<vector<int>>* edges;
+set<int>* labels;
+int* result;
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     int total;
-    double x,y,z;
-    cin>>total;
-    Eigen::Vector3d* d =new Eigen::Vector3d[total];
+//    cin>>total;
+    total = 60;
+    d = new Eigen::Vector3d[total];
+
+//    d[0]<<0,0,1;
+//    d[1]<<0,1,0;
+//    d[2]<<1,0,0;
+//    d[3]<<0,0,-1;
+//    d[4]<<0,-1,0;
+//    d[5]<<-1,0,0;
     for(int i = 0;i < total;i++){
-        cin>>x>>y>>z;
-        d[i]<<x,y,z;
+        d[i](0) = Guass();
+        d[i](1) = Guass();
+        d[i](2) = Guass();
+        d[i].normalize();
     }
     
     // Load a mesh in OFF format
-    igl::readOFF( "/Users/Lagrant/libigl/tutorial/shared/fandisk.off", V, F);
+    igl::readOFF( "/Users/Lagrant/libigl/tutorial/shared/bearing.off", V, F);
     
     // Compute per-face normals
     igl::per_face_normals(V,F,N_faces);
     int NfaceRows = N_faces.rows();
     int faceNum = F.rows();
     
+    // To apply the halfedge data structure
+    int totalVer = faceNum*3;
+    vertex* ver = (vertex*) malloc(sizeof(vertex)*totalVer);
+    edge* e = (edge*) malloc(sizeof(edge)*totalVer);
+    face* triFace = (face*) malloc(sizeof(face)*faceNum);
+    
+    Eigen::RowVector3d* color = new Eigen::RowVector3d[total];
+    result = (int*) malloc(sizeof(int)*faceNum);
+    
     //initialize the set class
     void* rawMemory = operator new(faceNum*sizeof(set<int>));
     void* rawMemory1 = operator new(faceNum*sizeof(set<vector<int>>));
-    void* rawMemory2 = operator new(faceNum*sizeof(set<vector<int>>));
-    set<int>* components = reinterpret_cast<set<int>*>(rawMemory);
-    set<vector<int>>* edges = reinterpret_cast<set<vector<int>>*>(rawMemory1);
-    set<vector<int>>* seams = reinterpret_cast<set<vector<int>>*>(rawMemory2);
+//    void* rawMemory2 = operator new(faceNum*sizeof(set<vector<int>>));
+    void* rawMemory3 = operator new(NfaceRows*sizeof(set<int>));
+    components = reinterpret_cast<set<int>*>(rawMemory);
+    edges = reinterpret_cast<set<vector<int>>*>(rawMemory1);
+//    set<vector<int>>* seams = reinterpret_cast<set<vector<int>>*>(rawMemory2);
+    labels = reinterpret_cast<set<int>*>(rawMemory3);
     for(int i = 0;i < faceNum;i++){
         new (&components[i])set<int>(faceNum); //NfaceRows = faceNum, NfaceRows means the total numebr of norms of faces while faceNum means the total  number of faces
         new (&edges[i])set<vector<int>>(3*faceNum);
-        new (&seams[i])set<vector<int>>(3*faceNum);
+//        new (&seams[i])set<vector<int>>(3*faceNum);
+        new (&labels[i])set<int>(faceNum);
     }
-    
+    printf("face number = %d\n", faceNum);
+
+
+//    color[0]<<0,0,1;
+//    color[1]<<0,1,0;
+//    color[2]<<1,0,0;
+//    color[3]<<0,1,1;
+//    color[4]<<1,0,1;
+//    color[5]<<1,1,0;
+    for(int i = 0;i < total;i++){
+        color[i](0) = (double) rand() / RAND_MAX;
+        color[i](1) = (double) rand() / RAND_MAX;
+        color[i](2) = (double) rand() / RAND_MAX;
+        color[i].normalize();
+    }
+    Eigen::MatrixXd C(faceNum,3);
     
     //compute the components class, the components are not connected yet
-    heightField(N_faces, d, components, edges, seams, F, total);
+    heightField(total);
     
-   
+    GeneralGraph_DArraySArraySpatVarying(faceNum,total);
+    
+    getHalfEdge(V.rows(), faceNum, result, &ver, &e, &triFace);
+    
+    build(&ver, totalVer);
+    
+    integrate(&triFace, result, faceNum);
+    
+    for(int i = 0 ;i < faceNum;i++){
+//        C.row(i) = color[labels[i].visitItem(0)];
+        C.row(i) = color[result[i]];
+    }
+    
     
     
     // Plot the mesh
@@ -56,7 +113,7 @@ int main(int argc, char *argv[])
     viewer.core.show_lines = false;
     viewer.data.set_mesh(V, F);
     viewer.data.set_normals(N_faces);
-    
+    viewer.data.set_colors(C);
     
     
     viewer.launch();
@@ -64,10 +121,18 @@ int main(int argc, char *argv[])
     for(int i = 0;i < faceNum;i++){
         components[i].~set();
         edges[i].~set();
-        seams[i].~set();
+//        seams[i].~set();
+        labels[i].~set();
     }
     operator delete(rawMemory);
     operator delete(rawMemory1);
-    operator delete(rawMemory2);
+    operator delete(rawMemory3);
+    delete[] color;
+    delete[] d;
+    free(result);
+    free(ver);
+    free(e);
+    free(triFace);
+    
     return 0;
 }
