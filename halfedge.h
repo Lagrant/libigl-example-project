2b277f40cf14a92ed76cf16eafa6790836dad6c0
Chuang Lagrant <chuanglagrant@gmail.com>
2018-02-01 10:04:21 +0800

    applying the half edge data structure

---
+++ halfedge.h
@@ -0,0 +1,45 @@
+//
+//  halfedge.h
+//  example
+//
+//  Created by 张鑫禹 on 2018/1/29.
+//
+//
+
+#ifndef halfedge_h
+#define halfedge_h
+#include <stdio.h>
+#include <Eigen/LU>
+struct edge;
+
+struct vertex {
+    int x;
+    int y;
+    int z;
+    
+    edge* startEdge;
+};
+
+struct face {
+    edge* adjacentEdge;
+    int numbering;
+    int label = -1;
+};
+
+struct edge {
+    vertex* endVertex;
+    edge* pair = NULL;
+    edge* next;
+    face* face;
+};
+bool equal(vertex* vi, vertex* vj);
+
+void assignEdge(vertex** ver, int end, edge** e, int i);
+
+void getHalfEdge(int verNum, int faceNum, int* result, vertex** ver,  edge** e, face** triFace);
+
+void build(vertex** ver, int totalVer);
+
+//void adjacentFace(face* f);
+
+#endif /* halfedge_h */
